fe82b1eb8a239362e59779b094a3bcf612e8b6ff
Justin Visser <jhivsser@uwaterloo.ca>
2014-08-15 14:17:01 -0400

    Adding pop-up box placeholder and page detection to optimize the extension more

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
+var pageTitle = $(document).find("title").text();
+var regex = /^Schedule/; 
 
-var content = '<input type="button" id="export" name="export" value="Export Schedule" class="shortButton" accesskey="E">';
-$(".submit").append(content);
+if(regex.test(pageTitle)) {
+	var content = '<input type="button" id="export" name="export" value="Export Schedule" class="shortButton" accesskey="E"><div class="exportPopup">This is a test</div>';
+	$(".submit").append(content);
 
-$("#export").click(function() {
-	if(typeof delimitedMeetingInfo !== 'undefined') {
-		if(delimitedMeetingInfo != '') {
-			alert("HERE");
+	$("#export").click(function() {
+		if(typeof delimitedMeetingInfo !== 'undefined') {
+			if(delimitedMeetingInfo != '') {
+				$(".exportPopup").fadeIn("slow");
+			}
+			else error();
 		}
 		else error();
-	}
-	else error();
-});
+	});
+}
 
 function error() {
 	alert("The data has not yet been retrieved. Please hit the button again in a few seconds!");
