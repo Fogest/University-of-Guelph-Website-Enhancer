1649dcabf5923945807cfce1d21eb35507bdb148
Justin Visser <justin@jhvisser.com>
2014-08-21 22:04:25 -0400

    Exporting the calendar to an ical now successful works

--- script.js
+++ script.js
@@ -1,44 +1,125 @@
 var pageTitle = $(document).find("title").text();
-var regex = /^Schedule/; 
+var regex = /^Schedule/;
 
-if(regex.test(pageTitle)) {
-	var content = '<input type="button" id="export" name="export" value="Export Schedule" class="shortButton" accesskey="E"><div class="exportPopup">This is a test</div>';
-	$(".submit").append(content);
+if (regex.test(pageTitle)) {
+    var content = '<input type="button" id="export" name="export" value="Export Schedule" class="shortButton" accesskey="E">' +
+        '<div id="exportPopup"><span class="title">Download iCal file</span></br>' +
+        '<span class="subtitle">(You can export this to most calendar applications)</span></br>' +
+        '<button type="button" class="button" id="icalDownload">Download</button></br>' +
+        '<button type="button" class="button" id="closeExportWindow">Close</button>' +
+        '</div>';
+    $(".submit").append(content);
 
-	$("#export").click(function() {
-		if(typeof delimitedMeetingInfo !== 'undefined') {
-			if(delimitedMeetingInfo != '') {
-				$(".exportPopup").fadeIn("slow");
-			}
-			else error();
-		}
-		else error();
-	});
+    $("#export").click(function () {
+        if (typeof delimitedMeetingInfo !== 'undefined') {
+            if (delimitedMeetingInfo != '') {
+                $("#exportPopup").fadeIn("slow");
+            } else error();
+        } else error();
+    });
+
+    $("#icalDownload").click(function () {
+        createDownload();
+    });
+
+    $("#closeExportWindow").click(function () {
+        $("#exportPopup").fadeOut("slow");
+    });
 }
 
 function error() {
-	alert("The data has not yet been retrieved. Please hit the button again in a few seconds!");
+    alert("The data has not yet been retrieved. Please hit the button again in a few seconds!");
+}
+
+
+function createDownload() {
+    var ical = [createAllEvents(getFormattedDataArray())];
+    var download = new Blob(ical, {
+        type: 'text/calendar'
+    });
+    var url = URL.createObjectURL(download);
+    window.location.replace(url);
 }
 
 function createAllEvents(data) {
-	var results = '';
-	for(i=0;i < data.length;++i) {
-		if(getCourseType(data[i]) != 'EXAM')
-			results += createIndividualIcalEvent(data[i]);
-        }
-	return results;
+    var results = 'BEGIN:VCALENDAR\n';
+    results += 'VERSION:2.0\n';
+    results += 'PRODID:-//Justin Visser/jhvisser.com//iCal Export v1.0//EN\n';
+    for (var i = 0; i < data.length; ++i) {
+        if (getCourseType(data[i]) != 'EXAM') results += createIndividualIcalEvent(data[i]);
+    }
+    results += 'END:VCALENDAR';
+    return results;
 }
 
 function createIndividualIcalEvent(data) {
-        var eventIcal = 'BEGIN:VEVENT\n';
-        eventIcal += 'DTSTART:' + toICSFormat(getCourseStart(data),getCourseTime(data,'start')) + '\n';
-        eventIcal += 'DTEND:' + toICSFormat(getCourseStart(data),getCourseTime(data,'end')) + '\n';
-        eventIcal += 'RRULE:FREQ=WEEKLY;UNILT=20141129T000000;WKST=SU;BYDAY=MO,TU\n';
-        eventIcal += 'SUMMARY:' + getCourseCode(data) + '('+ getCourseType(data) + ')' + '\n';
-        eventIcal += 'LOCATION:' + getCourseLocation(data) + ', University of Guelph\n';
-        eventIcal += 'DESCRIPTION:' + getCourseCode(data) + ' this is a much longer description....\n';
-        eventIcal += 'END:VEVENT\n';
-        return eventIcal;
+    var eventIcal = 'BEGIN:VEVENT\n';
+    eventIcal += 'DTSTART:' + correctStartTime(data) + '\n';
+    //eventIcal += 'DTEND:' + toICSFormat(getCourseStart(data), getCourseTime(data, 'end')) + '\n';
+    eventIcal += 'RRULE:FREQ=WEEKLY;UNTIL=20141129T000000;WKST=SU;BYDAY=' + convertArrayOfDatesToICSFormat(getArrayOfDates(getCourseDates(data))) + '\n';
+    eventIcal += 'SUMMARY:' + getCourseCode(data) + '(' + getCourseType(data) + ')' + '\n';
+    eventIcal += 'LOCATION:' + getCourseLocation(data) + ', University of Guelph\n';
+    eventIcal += 'DESCRIPTION:' + getCourseCode(data) + ' this is a much longer description....\n';
+    eventIcal += 'END:VEVENT\n';
+    return eventIcal;
+}
+
+function correctStartTime(data) {
+    var re = /\B(AM|PM)/gm;
+    var subst = ' $1';
+
+    var fullDate = getCourseStart(data) + ' ' + getCourseTime(data, 'start');
+    var originalDate = new Date(fullDate.replace(re, subst));
+    var date = new Date(getCourseStart(data));
+    var sameDay = false;
+    var dates = getArrayOfDates(getCourseDates(data));
+    for (var i = 0; i < dates.length; ++i) {
+        if (getDateVal(dates[i]) == date.getDay()) {
+            sameDay = true;
+            break;
+        }
+    }
+    if (sameDay == false) {
+        while (getDateVal(dates[0]) != date.getDay()) {
+            date.setDate(date.getDate() + 1);
+        }
+    }
+    var pre = date.getFullYear().toString() + ((date.getMonth() + 1) < 10 ? "0" + (date.getMonth() + 1).toString() : (date.getMonth() + 1).toString()); 
+    pre += ((date.getDate() + 1) < 10 ? "0" + date.getDate().toString() : date.getDate().toString());
+
+var post = originalDate.getHours() < 10 ? ("0" + originalDate.getHours().toString()) : (originalDate.getHours().toString());
+post += originalDate.getMinutes() < 10 ? ("0" + originalDate.getMinutes().toString()) : (originalDate.getMinutes().toString() + "00");
+    return pre + "T" + post;
+}
+
+function getDateVal(input) {
+    var otherVal = 0;
+    switch (input) {
+        case 'Mon':
+            otherVal = 1;
+            break;
+        case 'Tues':
+            otherVal = 2;
+            break;
+        case 'Wed':
+            otherVal = 3;
+            break;
+        case 'Thur':
+            otherVal = 4;
+            break;
+        case 'Fri':
+            otherVal = 5;
+            break;
+        case 'Sat':
+            otherVal = 6;
+            break;
+        case 'Sun':
+            otherVal = 0;
+            break;
+        default:
+            otherVal = 0;
+    }
+    return otherVal;
 }
 
 
@@ -57,71 +138,80 @@ The array looks like the following in each position:
 
 */
 
-function toICSFormat(date, time) {
-    var timeCont = [],
-        dateCont = [];
-
-    if (time.toLowerCase().indexOf('pm') != -1) {
-
-        timeCont = time.toLowerCase().replace('pm', 00).split(':'); //assuming from your question seconds is never mentioned but only hh:mm i.e. hours and minutes
-        timeCont[0] = (parseInt(timeCont[0]) + 12) % 24;
-    } else {
-        timeCont = time.toLowerCase().replace('am', 00).split(':');
-    }
-    dateCont = date.split('/');
-
-    return dateCont.join('') + 'T' + timeCont.join('');
-}
-
-function exportToIcal() {
-	var data = getFormattedDataArray();
-	console.log(getCourseCode(data[0]));
-	console.log(getCourseStart(data[0]));
-	console.log(getCourseEnd(data[0]));
-	console.log(getCourseType(data[0]));
-	console.log(getCourseDates(data[0]));
-	console.log(getCourseTime(data[0]));
-	console.log(getCourseLocation(data[0]));
-}
-
 function getFormattedDataArray() {
-	var results = [];
-	for(i=0;i < delimitedMeetingInfo.length;++i) {
-		results.push(delimitedMeetingInfo[i].split("|"));
-	}
-	return results;
+    var results = [];
+    for (var i = 0; i < delimitedMeetingInfo.length; ++i) {
+        results.push(delimitedMeetingInfo[i].split("|"));
+    }
+    return results;
+}
+
+function convertArrayOfDatesToICSFormat(date) {
+    var results = [];
+    for (var i = 0; i < date.length; ++i) {
+        switch (date[i]) {
+            case 'Mon':
+                results.push('MO');
+                break;
+            case 'Tues':
+                results.push('TU');
+                break;
+            case 'Wed':
+                results.push('WE');
+                break;
+            case 'Thur':
+                results.push('TH');
+                break;
+            case 'Fri':
+                results.push('FR');
+                break;
+            case 'Sat':
+                results.push('SA');
+                break;
+            case 'Sun':
+                results.push('SU');
+                break;
+            default:
+                results.push('Days of the week incorrect');
+        }
+    }
+    return results.join();
+}
+
+function getArrayOfDates(date) {
+
+    return date.replace(/\s/g, "").split(",");
 }
 
 function getCourseCode(data) {
-	return trim(data[1]);
+    return data[1];
 }
 
 function getCourseStart(data) {
-	return trim(data[2]);
+    return data[2];
 }
 
 function getCourseEnd(data) {
-	return trim(data[3]);
+    return data[3];
 }
 
 function getCourseType(data) {
-	return trim(data[4]);
+    return data[4];
 }
 
 function getCourseDates(data) {
-	return trim(data[5]);
+    return data[5];
 }
 
-function getCourseTime(data,startOrEnd) {
-	var re = /^((?:0[1-9]|1[012]):[0-5][0-9][AP]M) - ((?:0[1-9]|1[012]):[0-5][0-9][AP]M)$/; 
-	var match = re.exec(data[6]);
-	if(startOrEnd == 'start') {
-		return trim(match[1]);
-	} else if(startOrEnd == 'end') {
-		return trim(match[2]);
-	}
+function getCourseTime(data, startOrEnd) {
+    var re = /^((?:0[1-9]|1[012]):[0-5][0-9][AP]M) - ((?:0[1-9]|1[012]):[0-5][0-9][AP]M)$/;
+    var match = re.exec(data[6]);
+    if (startOrEnd == 'start') {
+        return match[1];
+    } else if (startOrEnd == 'end') {
+        return match[2];
+    }
 }
 
 function getCourseLocation(data) {
-	return trim(data[7]);
-}
+    return data[7];
